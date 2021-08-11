21786fe5c8542aa0e8f6e08f56d370fa6b319a09
Gustavo <gustavoadolfo@garciaparedes.edu.co>
2021-08-10 20:26:05 -0500

    Pagina About

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <b-row>
+        <b-col cols="4">
+        <div class="card text-white bg-dark shadow-lg" style="border-radius: 15px; height:100%; min-width:330px;" >
+            <div class="d-flex justify-content-center py-3">
+            <!-- v-bind para llamar propiedades dinamicas -->
+            <img
+                v-bind:src="member.image"
+                alt="Foto del equipo"
+                height="150"
+                width="auto"
+                style="border-radius: 15px"
+            />
+            </div>
+
+            <div class="card-body">
+            <h3 class="card-title text-center">{{ member.nombre }}</h3>
+            <p class="card-text pt-3">{{ member.descripcion }}</p>
+            <p class="card-text"><span>Rol: </span> {{ member.rol }}</p>
+            <!-- <p class="card-text"><span>Codigo: </span> {{ member. }}</p> -->
+          </div>
+        </div>
+        </b-col>
+        <b-col cols="4"></b-col>
+        <b-col cols="4"></b-col>
+    </b-row>
+  
+  
+    <!-- <div class="container" id="team"> -->
+      <!-- <div class="row justify-content mb-5"> -->
+
+          
+
+</template>
+
+<script>
+export default {
+  name: "TeamCard",
+  props: ["member"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,63 @@
 <template>
-  <div class="about">
+  <div class="container">
     <nav-bar></nav-bar>
-    <h1>This is an about page</h1>
-    <the-footer></the-footer>
+    <div class="container">
+      <h1 class="mt-1 text-center">ACERCA DEL EQUIPO</h1>
+    </div>
+      <!-- Area de miembros  -->
+    <div class="container-fluid mt-5 mb-5">
+      <div class="row justify-content-center">
+        <div class="col mt-2" v-for="(item, index) of team" :key="index">
+          <!-- v-bind se puede abreviar como :member solamente -->
+          <team-card v-bind:member="item"></team-card>
+        </div>
+      </div>
+    </div>
+  <the-footer></the-footer>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 import TheFooter from '@/components/TheFooter.vue'
+import TeamCard from '@/components/TeamCard.vue'
+
 export default {
   name:'about',
   components:{
     NavBar,
+    TeamCard,
     TheFooter,
-
-  }
+  },
+  data() {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: "Gustavo A. Gómez C.",
+          descripcion:
+            "Ingeniero Civil",
+          rol: "Desarrollador Backend",
+          image: "./imagenes/gustav.jpeg",
+        },
+        {
+          codigo: 2,
+          nombre: "Alejandro Peláez",
+          descripcion:
+            "Ingeniero Físico",
+          rol: "Desarrollador backend",
+          image: "./imagenes/photo.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "Ivan Aldana",
+          descripcion:
+            "Ingeniero Petróleos",
+          rol: "Desarrollador backend",
+          image: "./imagenes/photo.jpg",
+        },
+      ],
+    };
+  },
 }
 </script>
